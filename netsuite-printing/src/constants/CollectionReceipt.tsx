430372d801c555b55308f79978128226f2b88081
noreenswan10 <112919537+noreenswan10@users.noreply.github.com>
2024-12-11 15:03:04 +0800

    Update collectionreceipt.tsx

--- netsuite-printing/src/constants/CollectionReceipt.tsx
+++ netsuite-printing/src/constants/CollectionReceipt.tsx
@@ -3,6 +3,8 @@ import { useAuth } from "@/context/authcontext";
 import { PrintPageProps } from "@/types/types";
 import Size1Collection from "@/utils/collectionreceipt/size1";
 import Size2Collection from "@/utils/collectionreceipt/size2";
+import Size3Collection from "@/utils/collectionreceipt/size3";
+import Size4Collection from "@/utils/collectionreceipt/size4";
 
 const CollectionReceipt: React.FC<PrintPageProps> = ({ data }) => {
   const { user } = useAuth();
@@ -10,13 +12,91 @@ const CollectionReceipt: React.FC<PrintPageProps> = ({ data }) => {
   return (
     <div>
       {(() => {
-        const size1 = ["JIME", "MADRI"];
-        const size2 = ["GENT", "INIT2"];
+        const size1 = [
+          "JIME",
+          "MADRI",
+          "OROD",
+          "DSMBN",
+          "DSMBN",
+          "DSMSB",
+          "MARA2",
+          "BALA",
+          "DIPD",
+          "SALA",
+          "DSMV",
+          "CARC2",
+          "LIPA",
+          "DSMDB",
+          "DSMCA",
+          "RIZD",
+          "AURD",
+          "JIMEDSM",
+          "ILID",
+          "DATH",
+          "KATI",
+          "MOLD2",
+          "DSMTA",
+          "BONI",
+          "DSMTG",
+          "SDAV",
+          "DAPI",
+          "CARMB",
+          "LABD",
+          "ILIG",
+          "VITA",
+          "BANTA",
+          "CATAR",
+          "DIPD2",
+          "IPID",
+          "MOLD",
+          "SUCD",
+          "DSMK",
+          "TALI",
+          "BALAM",
+          "CALD",
+          "DSMPO",
+          "YATI",
+          "ALEN",
+          "BINAN",
+          "LILD",
+          "OROH",
+          "SIND",
+          "TUBOD",
+          "BULU",
+          "MARD",
+          "MEDE",
+          "ALIC",
+          "IMED",
+          "LOAY",
+          "MOLDS",
+          "MALA",
+          "VILLA",
+          "CARC",
+          "GUIN2",
+          "REMI",
+        ];
+        const size2 = [
+          "GENT",
+          "SANT2",
+          "SAGBA",
+          "TALI2",
+          "SANM",
+          "TANZ2",
+          "ALAD",
+          "TANZ3",
+          "INIT2",
+        ];
+        const size3 = ["BOLOD", "MING", "DANAO", "TOMAS", "CATAR2", "CARMC2"];
+        const size4 = ["SANP", "FAMY", "LAPU", "ANTIP", "SROS", "UBAY", "NAIC"];
 
         if (size1.some((branch) => branch === user?.branchCode)) {
           return <Size1Collection data={data} />;
         } else if (size2.some((branch) => branch === user?.branchCode)) {
           return <Size2Collection data={data} />;
+        } else if (size3.some((branch) => branch === user?.branchCode)) {
+          return <Size3Collection data={data} />;
+        } else if (size4.some((branch) => branch === user?.branchCode)) {
+          return <Size4Collection data={data} />;
         } else {
           return <>None</>;
         }
