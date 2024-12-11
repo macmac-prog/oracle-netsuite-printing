fc9ebbda0710bc98a0bade959d40063f669ce029
noreenswan10 <112919537+noreenswan10@users.noreply.github.com>
2024-12-11 13:57:50 +0800

    Additional

--- netsuite-printing/src/constants/CashSalesInvoice.tsx
+++ netsuite-printing/src/constants/CashSalesInvoice.tsx
@@ -1,18 +1,23 @@
 import { useAuth } from "@/context/authcontext";
 import { PrintPageProps } from "@/types/types";
-import Size1 from "@/utils/collectionreceipt/size1";
+import Size1Sales from "@/utils/salesinvoice/size1";
 
 const CashSalesInvoice: React.FC<PrintPageProps> = ({ data }) => {
   const { user } = useAuth();
 
   return (
   <div>
-    {["DSMC", "INAB"].some(branch => branch === user?.branchCode) ? (
-      <Size1 data={data} />
-    ) : (
-      <>None</>
-    )}
+    {(() => {
+      const size1 = ["DSMC", "INAB"];
+      
+      if (size1.some((branch) => branch === user?.branchCode)) {
+        return <Size1Sales data={data} />;
+      } else {
+        return <>None</>;
+      }
+    })()}
   </div>
+  
   );
 };
 
