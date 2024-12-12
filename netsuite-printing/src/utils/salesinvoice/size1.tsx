d209843541dc18377b10a18871bfb131efebe780
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2024-12-12 09:36:10 +0800

    Size 1 design
    Size 1 design

--- netsuite-printing/src/utils/salesinvoice/size1.tsx
+++ netsuite-printing/src/utils/salesinvoice/size1.tsx
@@ -24,33 +24,38 @@ const Size1Sales: React.FC<PrintPageProps> = ({ data }) => {
   const transactionTotal = 19;
 
   return (
-    <div className="text-xs h-[774.7px] w-[600.5px] border">
-      <div className="w-full flex mt-[139.8px]">
-        <p className="ml-[109.6px]">Sold to No data</p>
-        <p className="ml-[321.2px]">{data[date]?.[7]}</p>
+    <div className="text-xs h-[774.7px] w-[601px] border">
+      <div className="flex mt-[139.8px]">
+        <p className="w-[378px] pl-[95px]">Sold to No data</p>
+        <p className="w-[223px] pl-[53px]">{data[date]?.[7]}</p>
       </div>
-      <div className="w-full flex">
-        <p className="ml-[109.6px]">Tin No data</p>
-        <p className="ml-[321.2px]">Terms</p>
+      <div className="flex">
+        <p className="w-[378px] pl-[95px]">Tin No data</p>
+        <p className="w-[223px] pl-[53px]">Terms</p>
       </div>
-
-      <div className="w-full flex">
-        <p className="ml-[109.6px]">Address</p>
-        <p className="ml-[321.2px]">OSCA/PWD ID NO</p>
+      <div className="flex h-[29px]">
+        <p className="w-[378px] pl-[95px]">Address</p>
+        <p className="w-[223px] pl-[136px]">104</p>
       </div>
-      <table className="border-collapse w-full">
-        <tbody>
-          {data.slice(1).map((row, index) => (
-            <tr key={index} className="text-xs">
-              <td>{row[10]}</td>
-              <td>{row[11]}</td>
-              <td>{row[9]}</td>
-              <td>{row[13]}</td>
-              <td>{row[15]}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="flex">
+        <p className="w-[378px] pl-[129px]">Business style</p>
+        <p className="w-[223px] pl-[87px]">Sample sig</p>
+      </div>
+      <div className="mx-[26.4px] border mt-[23px]">
+        <table className="border-collapse w-full">
+          <tbody>
+            {data.slice(1).map((row, index) => (
+              <tr key={index} className="text-xs text-center">
+                <td className="w-[72px]">{row[10]}</td>
+                <td className="w-[53px]">{row[11]}</td>
+                <td className="w-[238.1px]">{row[9]}</td>
+                <td className="w-[76px]">{row[13]}</td>
+                <td className="w-[98.2px]">{row[15]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
