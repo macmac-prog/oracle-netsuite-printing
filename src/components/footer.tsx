c95e4117473643f6e4aa1c188d8208871ba5624a
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-02-17 11:11:54 +0800

    Footer Copy Right
    Footer Copy Right

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="w-full flex justify-center items-center py-5 bg-[#607799]">
       <p className="text-sm text-white text-center">
-        © 2024 All Rights Reserved | By SMCT WEB DEV with ❤
+        © 2024 - {new Date().getFullYear()} All Rights Reserved | By SMCT WEB DEV with ❤
         <br />
         Macmac | Irvin | Noreen | Jenecil | Allan
       </p>
