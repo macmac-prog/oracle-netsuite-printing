d6c1e9439f74ab85e6f34d3c3c4d9975716b45fc
Mark Anthony Bulala <117882831+macmac-prog@users.noreply.github.com>
2025-02-21 17:24:20 +0800

    Update HondaDESSize.tsx

--- src/utils/collectionreceipt/global/HondaDESSize.tsx
+++ src/utils/collectionreceipt/global/HondaDESSize.tsx
@@ -16,26 +16,26 @@ const HondaDESSize: React.FC<PrintPageProps> = ({ data }) => {
 
   return (
     <div className="text-xs w-[769.13385827px] h-[396.8503937px] border">
-        <div className="flex h-[19px] mt-[143.6220472406px]">
+        <div className="flex h-[19px] mt-[109.6220472406px]">
           <p className="pl-[604.72440945px] w-[143.62204724px]">{data[1]?.[CR_Date]}</p>
         </div>
-        <div className="flex justify-between h-[19px] mt-[37.7952755906px]">
+        <div className="flex justify-between h-[19px] mt-[19.7952755906px]">
           <p className="pl-[313.7007874px]">{data[1]?.[CR_Name]}</p>
           <p className="mr-[68px]">{data[1]?.[CR_TIN]}</p>
         </div>
-        <div className="flex h-[19px] mt-[18.897637795px]">
+        <div className="flex h-[19px]">
           <p className="ml-[272.12598425px]">{data[1]?.[CR_Address]}</p>
         </div>
-        <div className="flex h-[19px] mt-[18.897637795px]">
+        <div className="flex h-[19px]">
           <p className="ml-[272.12598425px]">
             {data[1]?.[CR_BusinessStyle]}
           </p>
         </div>
-        <div className="h-[19px] flex w-full mt-[18.897637795px]">
-          <p className="w-[366.61417323px] ml-[188.9763779528px]">{data[1]?.[CR_AmountInWords]}</p>
+        <div className="h-[19px] flex w-full">
+          <p className="w-[366.61417323px] ml-[225.9763779528px]">{data[1]?.[CR_AmountInWords]}</p>
           <p className="ml-[56.692913386px]">{data[1]?.[CR_AmountInFigures]}</p>
         </div>
-        <div className="h-[19px] flex w-[529px] mt-[18.897637795px] ml-[188.9763779528px]">
+        <div className="h-[19px] flex w-[529px] ml-[225.9763779528px]">
           <p>{data[1]?.[CR_Memo]}</p>
         </div>
     </div>
