6003faf68d423d152c797102845e45d05fd970e3
Mark Anthony Bulala <117882831+macmac-prog@users.noreply.github.com>
2025-02-21 17:11:38 +0800

    Update HondaDESSize.tsx

--- src/utils/collectionreceipt/global/HondaDESSize.tsx
+++ src/utils/collectionreceipt/global/HondaDESSize.tsx
@@ -15,19 +15,18 @@ const HondaDESSize: React.FC<PrintPageProps> = ({ data }) => {
   const CR_PartnerName = 9;
 
   return (
-    <div className="text-sm flex justify-evenly w-[769.13385827px] h-[396.8503937px] px-[34.015748031px]">
-      <div className="w-full mt-[105.82677165px]">
-        <div className="flex w-full mx-[585.82677165px]">
-          <p>{data[1]?.[CR_Date]}</p>
+    <div className="text-xs w-[769.13385827px] h-[396.8503937px] border">
+        <div className="flex h-[19px] mt-[143.6220472406px]">
+          <p className="pl-[604.72440945px] w-[143.62204724px]">{data[1]?.[CR_Date]}</p>
         </div>
         <div className="flex justify-between h-[19px] mt-[37.7952755906px]">
-          <p className="ml-[283.46456693px]">{data[1]?.[CR_Name]}</p>
+          <p className="pl-[313.7007874px]">{data[1]?.[CR_Name]}</p>
           <p className="mr-[68px]">{data[1]?.[CR_TIN]}</p>
         </div>
-        <div className="h-[19px] mt-[18.897637795px]">
+        <div className="flex h-[19px] mt-[18.897637795px]">
           <p className="ml-[272.12598425px]">{data[1]?.[CR_Address]}</p>
         </div>
-        <div className="h-[19px] mt-[18.897637795px]">
+        <div className="flex h-[19px] mt-[18.897637795px]">
           <p className="ml-[272.12598425px]">
             {data[1]?.[CR_BusinessStyle]}
           </p>
@@ -39,7 +38,6 @@ const HondaDESSize: React.FC<PrintPageProps> = ({ data }) => {
         <div className="h-[19px] flex w-[529px] mt-[18.897637795px] ml-[188.9763779528px]">
           <p>{data[1]?.[CR_Memo]}</p>
         </div>
-      </div>
     </div>
   );
 };
